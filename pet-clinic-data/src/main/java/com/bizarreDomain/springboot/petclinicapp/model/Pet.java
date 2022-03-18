f04b021e11cd79906a9dde2d0dd886cea5a08205
Aashrey Singh <aashreyofficial@gmail.com>
2022-03-18 12:19:17 +0530

    added BaseEntity to add id property to model classes

--- pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/model/Pet.java
+++ pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/model/Pet.java
@@ -2,7 +2,7 @@ package com.bizarreDomain.springboot.petclinicapp.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/model/PetType.java
+++ pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/model/PetType.java
@@ -1,6 +1,6 @@
 package com.bizarreDomain.springboot.petclinicapp.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
