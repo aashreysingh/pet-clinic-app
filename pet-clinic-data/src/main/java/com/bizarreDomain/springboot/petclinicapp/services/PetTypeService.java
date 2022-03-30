db6e00cf315706c7506d457f235efc2d7bcfb6d6
Aashrey Singh <aashreyofficial@gmail.com>
2022-03-31 02:35:30 +0530

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.bizarreDomain.springboot.petclinicapp.services;
+
+import com.bizarreDomain.springboot.petclinicapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.bizarreDomain.springboot.petclinicapp.services.map;
+
+import com.bizarreDomain.springboot.petclinicapp.model.PetType;
+import com.bizarreDomain.springboot.petclinicapp.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
