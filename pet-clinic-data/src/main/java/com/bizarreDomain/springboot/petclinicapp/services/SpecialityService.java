b40438e1493cc86a0bcd8bffae91b62b021bd234
Aashrey Singh <aashreyofficial@gmail.com>
2022-03-31 03:15:45 +0530

    added Speciality services (including Map Based Service)

---
+++ pet-clinic-data/src/main/java/com/bizarreDomain/springboot/petclinicapp/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.bizarreDomain.springboot.petclinicapp.services;
+
+import com.bizarreDomain.springboot.petclinicapp.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
